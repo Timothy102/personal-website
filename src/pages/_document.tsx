30624dc5cd38a5fb1a6de513e8fde0ad290ff7c8
Timothy102 <cvetko.tim@gmail.com>
2025-01-05 10:16:12 +0100

    google tag

---
+++ src/pages/_document.tsx
@@ -0,0 +1,25 @@
+// pages/_document.tsx
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        {/* Google Analytics */}
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-TQ43EW288T"></script>
+        <script dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-TQ43EW288T');
+          `
+        }} />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
